68feed8313ec091cc6301681ca15354a21324caa
Aleksandr Malakhov <7true@mail.ru>
2019-02-24 19:34:08 +0700

    text shifted

--- app/src/main/java/tk/alltrue/circletext/MyWidgetView.java
+++ app/src/main/java/tk/alltrue/circletext/MyWidgetView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -17,6 +19,8 @@ public class MyWidgetView extends View {
 
     private int mColor;
     private Paint mPaint;
+    private RectF mRectF;
+    private Path mPath;
 
     public MyWidgetView(Context context) {
         super(context);
@@ -39,6 +43,8 @@ public class MyWidgetView extends View {
         setMinimumHeight(MIN_HEIGHT);
         mColor = DEFAULT_COLOR;
         mPaint = new Paint();
+        mRectF = new RectF();
+        mPath = new Path();
     }
 
     @Override
@@ -51,6 +57,18 @@ public class MyWidgetView extends View {
         mPaint.setColor(mColor);
         mPaint.setStrokeWidth(STROKE_WIDTH);
         canvas.drawRect(5, 5, getWidth() - 5, getHeight() - 5, mPaint);
+
+        mPaint.setColor(Color.GRAY);
+        mPaint.setTextSize(20);
+
+        mRectF.set(22, 22, getWidth()-22, getHeight()-22);
+        mPath.addArc(mRectF, 180,90);
+
+        canvas.drawTextOnPath("Crazy", mPath, 0, 0, mPaint);
+        mPath.reset();
+
+        mPath.addArc(mRectF, 0, 120);
+        canvas.drawTextOnPath("rectangle", mPath, 0, 0, mPaint);
     }
 
     public void setColor(int color) {
